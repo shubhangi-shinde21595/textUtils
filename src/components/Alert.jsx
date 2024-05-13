5980a0ea15b615701c5ef85caef5acac5f58f845
Shubhangi Shinde <158753518+shubhangi-shinde21595@users.noreply.github.com>
2024-05-13 13:10:21 +0530

    first commit

---
+++ src/components/Alert.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Alert = ({ alert }) => {
+  const capitalize = (word) => {
+    const lower = word.toLowerCase();
+    return lower.charAt(0).toUpperCase() + lower.slice(1);
+  };
+  return (
+    alert && (
+      <div
+        className={`alert alert-${alert.type} alert-dismissible fade show`}
+        role="alert"
+      >
+        <strong>{capitalize(alert.type)}</strong> : {alert.msg}
+      </div>
+    )
+  );
+};
+
+export default Alert;
---
+++ src/components/TextForm.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+export default function TextForm({ heading, mode, showAlert }) {
+  const [text, setText] = useState("Enter Text Here...");
+  function handelUpClick() {
+    let newText = text.toUpperCase();
+    setText(newText);
+    showAlert("Converted to UpperCase!", "primary");
+  }
+  function handellowClick() {
+    let newText = text.toLowerCase();
+    setText(newText);
+    showAlert("Converted to LowerCase!", "warning");
+  }
+  function handleremClick() {
+    let newText = text.replace(/\s+/g, " ").trim();
+    setText(newText);
+    showAlert("Remove extra blanck space!", "info");
+  }
+  function handleClearClick() {
+    let newText = " ";
+    setText(newText);
+    showAlert("Cleared all text!", "danger");
+  }
+  function handleOnchange(event) {
+    setText(event.target.value);
+  }
+
+  return (
+    <>
+      <div
+        className="container"
+        style={{
+          color: `${mode === "dark" ? "white" : "black"}`,
+        }}
+      >
+        <h1>{heading}</h1>
+        <div className="mb-3">
+          <textarea
+            style={{
+              backgroundColor: `${mode === "dark" ? "grey" : "white"}`,
+              color: `${mode === "dark" ? "white" : "black"}`,
+            }}
+            className="form-control"
+            value={text}
+            id="myBox"
+            rows="10"
+            onChange={handleOnchange}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2 my-2" onClick={handelUpClick}>
+          Convert to UpperCase
+        </button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handellowClick}>
+          Convert to LowerCase
+        </button>
+        <button className="btn btn-primary mx-2 my-2" onClick={handleremClick}>
+          Remove Extra Space
+        </button>
+        <button
+          className="btn btn-primary mx-2 my-2"
+          onClick={handleClearClick}
+        >
+          Clear Text
+        </button>
+      </div>
+      <div
+        className="container my-2"
+        style={{
+          color: `${mode === "dark" ? "white" : "black"}`,
+        }}
+      >
+        <h1>Your text Summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes to read </p>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text : "Enter something here to preview"}</p>
+      </div>
+    </>
+  );
+}
